cddd9f768220d49f6d78fc853e3e45120a2434e8
mustaphazahiri <zahir1979@gmail.com>
2022-11-03 14:31:09 +0100

    admin can activate account and modify user role

--- models/Administrateur/Administrateur.model.php
+++ models/Administrateur/Administrateur.model.php
@@ -5,7 +5,7 @@ class AdministrateurManager extends MainManager
 {
     public function getUtilisateurs()
     {
-        $req = $this->getBdd()->prepare("SELECT is_valid,nom_user,prenom_user,role.nom_role as id_role FROM user inner join role on user.id_role-role.id_role");
+        $req = $this->getBdd()->prepare("SELECT nom_user,prenom_user,is_valid, role.nom_role as id_role FROM user inner join role on user.id_role=role.id_role");
         $req->execute();
         $datas = $req->fetchAll(PDO::FETCH_ASSOC);
         $req->closeCursor();
--- views/Administrateur/droits.view.php
+++ views/Administrateur/droits.view.php
@@ -12,8 +12,30 @@
             <tr>
                 <td class="text-center"><?= $utilisateur['nom_user'] ?></td>
                 <td class="text-center"><?= $utilisateur['prenom_user'] ?></td>
-                <td class="text-center"><?= $utilisateur['id_role'] ?></td>
-                <td class="text-center"><?= (int)$utilisateur['is_valid'] === 0 ? "Non" : "Oui" ?></td>
+                <td class="text-center">
+                    <?php if ($utilisateur['id_role'] === "admin") : ?>
+                        <?= $utilisateur['id_role'] ?>
+                    <?php else : ?>
+                        <form method="POST" action="<?= URL ?>administration/validation_modificationRole">
+                            <input type="hidden" name="login" value="<?= $utilisateur['login'] ?>" />
+                            <select class="form-select" name="role" onchange="confirm('confirmez vous la modification ?') ? submit() : document.location.reload()">
+                                <option value="cuisinier" <?= $utilisateur['id_role'] === "cuisinier" ? "selected" : "" ?>>Cuisinier</option>
+                                <option value="livreur" <?= $utilisateur['id_role'] === "livreur" ? "selected" : "" ?>>Livreur</option>
+
+                            </select>
+                        </form>
+                    <?php endif; ?>
+                </td>
+                <td class="text-center">
+                    <form method="POST" action="<?= URL ?>administration/validation_compte">
+                        <input type="hidden" name="login" value="<?= $utilisateur['login'] ?>" />
+                        <select class="form-select" name="is_valid" onchange="confirm('confirmez vous la modification ?') ? submit() : document.location.reload()">
+                            <option value="1" <?= $utilisateur['is_valid'] == "1" ? "selected" : "" ?>>Oui</option>
+                            <option value="0" <?= $utilisateur['is_valid'] == "0" ? "selected" : "" ?>>Non</option>
+                        </select>
+                    </form>
+                </td>
+
             </tr>
         <?php endforeach; ?>
     </thead>
